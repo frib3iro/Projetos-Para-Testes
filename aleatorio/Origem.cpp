e6bdab3f0d378c927337977fa8630d9c8b9e7da5
Fabio Ribeiro <rib3iro@live.com>
2022-12-13 00:30:56 -0300

    Jogo Pronto

--- aleatorio/Origem.cpp
+++ aleatorio/Origem.cpp
@@ -5,15 +5,35 @@ using namespace std;
 
 const int TamVet = 6;
 int random();
+void Mega(int[], int);
+void BubbleSort(int[], int);
+void MostrarJogo(int[], int);
 
 int main()
 {
-	cout << "preenchendo cada elemento do vetor\n";
-
+	cout << "Criando um jogo da Mega Sena...\n\n";
 	int vet[TamVet] = {};
+	
+	Mega(vet, TamVet);
+	BubbleSort(vet, TamVet);
+	MostrarJogo(vet, TamVet);
+
+	system("pause");
+	return 0;
+}
+
+int random()
+{
+	random_device rd;
+	mt19937 mt(rd());
+	uniform_int_distribution<int> dist(1, 60);
+	return dist(mt);
+}
+
+void Mega(int vet[], int TamVet)
+{
 	int num = random();
 	int i = 0;
-
 	while(i < TamVet)
 	{
 		if(vet[i] == num)
@@ -33,33 +53,30 @@ int main()
 			i++;
 		}
 	}
-
-	int aux = 0;
-	for(int i = 0; i < TamVet; i++)
-	{
-		if(vet[i] < vet[i + 1])
-		{
-			aux = vet[i];
-			vet[i] = vet[i + 1];
-			vet[i + 1] = aux;
-		}
-	}
-
-	cout << endl;
-
-	cout << "Mostrando cada elemento do vetor\n";
-	for(int v : vet)
-		cout << internal << setw(2) << setfill('0') << v << " ";
-	cout << endl << endl;
-
-	system("pause");
-	return 0;
 }
 
-int random()
+void BubbleSort(int vet[], int TamVet)
 {
-	random_device rd;
-	mt19937 mt(rd());
-	uniform_int_distribution<int> dist(1, 60);
-	return dist(mt);
+	for(int i = 0; i < TamVet; ++i)
+	{
+		for(int j = i + 1; j < TamVet; ++j)
+		{
+			int aux = 0;
+			if(vet[i] > vet[j])
+			{
+				aux = vet[i];
+				vet[i] = vet[j];
+				vet[j] = aux;
+			}
+		}
+	}
+}
+
+void MostrarJogo(int vet[], int TamVet)
+{
+	for(int i = 0; i < TamVet; i++)
+	{
+		cout << internal << setw(2) << setfill('0') << vet[i] << " ";
+	}
+	cout << endl << endl;
 }
